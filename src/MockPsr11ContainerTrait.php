3bade2bbba3b1944c301d6fae2350878fabf7ea6
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2021-09-22 11:26:28 +0200

    Rename misnamed trait

---
+++ src/MockPsr11ContainerTrait.php
@@ -0,0 +1,31 @@
+<?php
+namespace tomkyle\MockPsr;
+
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+trait MockPsr11ContainerTrait
+{
+    use ProphecyTrait;
+
+    public function mockContainer(array $items = array()) : ContainerInterface
+    {
+        $NF = new class() extends \Exception implements NotFoundExceptionInterface {
+        };
+
+        $container_mock = $this->prophesize(ContainerInterface::class);
+        $container_mock->has(Argument::any())->willReturn(false);
+        $container_mock->get(Argument::any())->willThrow($NF);
+
+        foreach ($items as $key => $item) {
+            $container_mock->has(Argument::exact($key))->willReturn(true);
+            $container_mock->get(Argument::exact($key))->willReturn($item);
+        }
+
+
+        return $container_mock->reveal();
+    }
+}
